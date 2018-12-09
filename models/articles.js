d3822044e7dcd02d75f2078bebc18961b45c817d
nhantt <nhan.tran@navigosgroup.com>
2018-12-10 00:07:34 +0700

    update input

--- models/articles.js
+++ models/articles.js
@@ -1,13 +1,8 @@
 const mongoose = require('../libs/mongoose');
-const User = mongoose.model('User', { 
-  email: {type:String,unique:true} ,
-  password:String,
-  token:String
+const Articles = mongoose.model('Articles', { 
+  title: String ,
+  content:String,
+  thumbnail:String
 });
 
-const user = new User({ email: 'admin@yopmail.com',password:'$2y$08$D307KNhTGKbl/E15C/nxgeQjcaAj1htpZip7vtdRg1ghoiGptEw8q' }); //123456789
-user.save().then(() => console.log('Add default account')).catch(err=>{
-  console.log('Add default account')
-});
-
-module.exports = User;
+module.exports = Articles;
