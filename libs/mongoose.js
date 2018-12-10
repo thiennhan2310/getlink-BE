a1228c2ab1e2426800b236d4e50069f80745985b
nhantt <nhan.tran@navigosgroup.com>
2018-12-10 22:21:13 +0700

    update package json

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const host = process.env.MONGO_HOST || 'localhost:27017';
+const host = process.env.MONGO_HOST || 'thiennhan2310:AwfKYEmu92rLcbe@ds115533.mlab.com:15533';
 mongoose.connect(`mongodb://${host}/getlink`,{useNewUrlParser: true });
 
 module.exports = mongoose;
